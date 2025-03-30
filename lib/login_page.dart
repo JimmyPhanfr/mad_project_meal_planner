8fd49cf5b6527a68d8290c23051222571720880e
JimmyPhanfr <JimmyPhanfr@gmail.com>
2025-03-30 18:27:19 -0400

    merges page02 into main

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -29,7 +29,7 @@ class _LoginPageState extends State<LoginPage> {
   //function for logging in to the app, checks if the email and password entered match an for an existing user in the user database. Upon success, populates the recipe database, if the recipe database is already populated then no action is taken
   void _login(BuildContext context) async {
     if (_formKey.currentState!.validate()) {
-      final user = await UserDB().getUser(_emailController.text.trim(), _passwordController.text.trim()); //retrieves the user if there is a matching email and password in the database, otherwise returns null to indicate no matching email and password
+      final user = await UserDB().getUser(_emailController.text.trim().toLowerCase(), _passwordController.text.trim()); //retrieves the user if there is a matching email and password in the database, otherwise returns null to indicate no matching email and password
       if (user != null) {
         await LoadRecipes.instance.loadRecipes(); // populates the recipe database in case the database is not populated
         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Login Successful')));
